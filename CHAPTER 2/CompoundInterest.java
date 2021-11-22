39134d116dfe22df5f28880fa2de35e0b05dd6bc
michalb1804 <92114360+michalb1804@users.noreply.github.com>
2021-11-22 20:19:20 +0100

    Update CompoundInterest.java

--- CHAPTER 2/CompoundInterest.java
+++ CHAPTER 2/CompoundInterest.java
@@ -6,7 +6,31 @@ public class CompoundInterest
 		
 		for(int i = 0; i < years; i++)
 		{
-			multiplicationFactor *= (1 + annualInterestRate/100.0F);
+			multiplicationFactor *= (1 + annualInterestRate / 100.0F);
+		}
+		
+		return startingValue * multiplicationFactor;
+	}
+	
+	public static float calculateAfterMonths(float startingValue, float annualInterestRate, int months)
+	{
+		float multiplicationFactor = 1;
+		
+		for(int i = 0; i < months; i++)
+		{
+			multiplicationFactor *= (1 + (annualInterestRate / 12.0F) / 100.0F);
+		}
+		
+		return startingValue * multiplicationFactor;
+	}
+	
+	public static float calculateAfterDays(float startingValue, float annualInterestRate, int days)
+	{
+		float multiplicationFactor = 1;
+		
+		for(int i = 0; i < days; i++)
+		{
+			multiplicationFactor *= (1 + (annualInterestRate / 365.0F) / 100.0F);
 		}
 		
 		return startingValue * multiplicationFactor;
