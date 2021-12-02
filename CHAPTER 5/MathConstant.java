c3f08db57accbcd85020ab3739d63f2c47a53b9e
michalb1804 <92114360+michalb1804@users.noreply.github.com>
2021-12-02 12:38:29 +0100

    Create MathConstant.java

---
+++ CHAPTER 5/MathConstant.java
@@ -0,0 +1,43 @@
+public class MathConstant 
+{
+	public static double pi(int precision)
+	{
+		double accumulator = 0;
+		
+		for(int i = 1; i <= precision; i++)
+		{
+			accumulator += Math.pow(-1, i + 1) / ((2 * i) - 1);
+		}
+		
+		return 4 * accumulator;
+	}
+	
+	public static double e(int precision)
+	{
+		double accumulator = 0;
+		
+		for(int i = 1; i <= precision; i++)
+		{
+			if(i >= 34)
+			{
+				System.out.println("Reached maximal possible precision. Max precision is 33.");
+				break;
+			}
+			accumulator += 1.0 / factorial(i);
+		}
+		
+		return accumulator;
+	}
+	
+	private static int factorial(int number)
+	{
+		int result = 1;
+		
+		for(int i = 2; i <= number; i++)
+		{
+			result *= i;
+		}
+		
+		return result;
+	}
+}
