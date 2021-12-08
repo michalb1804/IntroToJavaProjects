e1ae77e1e9dc0032d0d11248a23a21b7263343dd
michalb1804 <92114360+michalb1804@users.noreply.github.com>
2021-12-08 18:59:12 +0100

    Update ArrayHelper.java

--- CHAPTER 7/ArrayHelper.java
+++ CHAPTER 7/ArrayHelper.java
@@ -19,6 +19,61 @@ public class ArrayHelper
 		return true;
 	}
 	
+	public static int[] mergeSorted(int[] array1, int[] array2)
+	{
+		int[] arrayMerged = new int[array1.length + array2.length];
+		int counter = 0;
+		
+		if(compareLength(array1, array2) == -1 || compareLength(array1, array2) == 0)
+		{
+			for(int i = 0; i < array2.length; i++)
+			{
+				arrayMerged[counter] = array1[i];
+				counter++;
+				
+				arrayMerged[counter] = array2[i];
+				counter++;
+			}
+			
+			for(int i = array2.length; i < array1.length; i++)
+			{
+				arrayMerged[counter] = array1[i];
+				counter++;
+			}
+		}
+		else
+		{
+			for(int i = 0; i < array1.length; i++)
+			{
+				arrayMerged[counter] = array1[i];
+				counter++;
+				
+				arrayMerged[counter] = array2[i];
+				counter++;
+			}
+			
+			for(int i = array1.length; i < array2.length; i++)
+			{
+				arrayMerged[counter] = array2[i];
+				counter++;
+			}
+		}
+		
+		SelectionSort.sort(arrayMerged);
+		
+		return arrayMerged;
+	}
+	
+	public static int compareLength(int[] array1, int[] array2)
+	{
+		if(array1.length > array2.length)
+			return -1;
+		else if(array1.length < array2.length)
+			return 1;
+		else
+			return 0;
+	}
+	
 	private static void swap(int[] array, int a, int b)
 	{
 		int tempValue = 0;
